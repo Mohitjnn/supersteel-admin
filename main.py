8532ced978d159ac1e0ace44536debf888ded149
Mohitjnn <jnmohit29@gmail.com>
2024-09-07 14:51:53 +0530

    authentication basic added and template creation done

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from starlette_admin import DropDown
 from starlette_admin import I18nConfig
 from starlette_admin.contrib.mongoengine import Admin
 from starlette_admin.views import Link
+from mongo_engine.auth import MyAuthProvider
+from starlette.middleware import Middleware
+from starlette.middleware.sessions import SessionMiddleware
 
 load_dotenv()
 
@@ -25,7 +28,9 @@ admin = Admin(
     route_name="admin-mongoengine",
     logo_url="https://preview.tabler.io/static/logo-white.svg",
     login_logo_url="https://preview.tabler.io/static/logo.svg",
-    templates_dir="templates/admin/mongoengine",
+    templates_dir="templates/",
+    auth_provider=MyAuthProvider(login_path="/sign-in", logout_path="/sign-out"),
+    middlewares=[Middleware(SessionMiddleware, secret_key="123456")],
     i18n_config=I18nConfig(language_switcher=["en", "fr"]),
 )
 
@@ -37,7 +42,7 @@ admin.add_view(
     )
 )
 
-admin.add_view(Link(label="Go Back to Home", icon="fa fa-link", url="/"))
+admin.add_view(Link(label="Go Back to Home", icon="fa fa-link", url="/product/list"))
 
 
 app = FastAPI()
